b58b3d32686d27f068ac71784f9c51d4576ed711
Aline <aline.niggeler@gmail.com>
2019-11-13 20:40:35 +0100

    Spring Data

--- camp/src/main/java/ch/bfh/springerstifu/heroes/config/ServiceConfiguration.java
+++ camp/src/main/java/ch/bfh/springerstifu/heroes/config/ServiceConfiguration.java
@@ -1,25 +1,66 @@
 package ch.bfh.springerstifu.heroes.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
 
 import ch.bfh.springerstifu.heroes.service.HeroService;
 import ch.bfh.springerstifu.heroes.service.PartyService;
 import ch.bfh.springerstifu.heroes.service.impl.HeroServiceImpl;
 import ch.bfh.springerstifu.heroes.service.impl.PartyServiceImpl;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 @Configuration
+@EnableJpaRepositories(basePackages = "ch.bfh.springerstifu.heroes.repository")
 public class ServiceConfiguration {
-    @Bean
+
     @Primary
+    @Bean
     public HeroService heroService() {
         return new HeroServiceImpl();
     }
-
-    @Bean
     @Primary
+    @Bean
     public PartyService partyService() {
         return new PartyServiceImpl();
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+    	return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+    	HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+    	bean.setDatabase(Database.H2);
+    	bean.setGenerateDdl(true);
+    	return bean;
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dSource, JpaVendorAdapter jva) {
+    	LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+    	bean.setDataSource(dSource);
+    	bean.setJpaVendorAdapter(jva);
+    	bean.setPackagesToScan("ch.bfh.springerstifu.heroes.model");
+    	return bean;
+    }
+    
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+    	return new JpaTransactionManager(emf);
     }
 }
