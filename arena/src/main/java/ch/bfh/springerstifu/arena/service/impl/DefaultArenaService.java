e21663357f83016741dcfa3864dc237337104a6b
markoczy <a.markoczy@gmail.com>
2020-01-27 20:30:22 +0100

    Improved battle speed

--- arena/src/main/java/ch/bfh/springerstifu/arena/service/impl/DefaultArenaService.java
+++ arena/src/main/java/ch/bfh/springerstifu/arena/service/impl/DefaultArenaService.java
@@ -3,6 +3,7 @@ package ch.bfh.springerstifu.arena.service.impl;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,25 +19,31 @@ public class DefaultArenaService implements ArenaService {
 	private static final Logger LOG = LoggerFactory.getLogger(DefaultArenaService.class);
 	private static final DecimalFormat f = new DecimalFormat("##.00");
 
+	private List<String> history;
+
 	@Override
 	public Party battle(Party challengeeParty, Party challengerParty) {
+		// as there is often a histrix timeout, we try to improve the speed by
+		// reducing log calls (now 90% of the frontend calls are ok, I didn't
+		// find out how to increase timeout)
+		history = new ArrayList<String>();
 
 		List<Hero> challengees = new ArrayList<>(challengeeParty.getMembers());
 		List<Hero> challengers = new ArrayList<>(challengerParty.getMembers());
 
-		LOG.info("Party '" + challengeeParty.getName() + "' fights against party '" + challengerParty.getName() + "'.");
+		history.add(
+				"Party '" + challengeeParty.getName() + "' fights against party '" + challengerParty.getName() + "'.");
 
 		int duelCount;
 		int roundCount = 0;
 
 		while (true) {
-
 			List<Hero> loosers = new ArrayList<>();
 			roundCount++;
 
-			LOG.info(
+			history.add(
 					"============================================================================================================");
-			LOG.info("Starting round no. " + roundCount);
+			history.add("Starting round no. " + roundCount);
 
 			// there can be only as much duels as the count of remaining members in the
 			// smallest party
@@ -58,18 +65,20 @@ public class DefaultArenaService implements ArenaService {
 			challengees.removeAll(loosers);
 			challengers.removeAll(loosers);
 
-			LOG.info("Party '" + challengeeParty.getName() + "' has " + challengees.size() + " members left.");
-			LOG.info("Party '" + challengerParty.getName() + "' has " + challengers.size() + " members left.");
+			history.add("Party '" + challengeeParty.getName() + "' has " + challengees.size() + " members left.");
+			history.add("Party '" + challengerParty.getName() + "' has " + challengers.size() + " members left.");
 
 			// check if a party has already lost (no members left), return the winners party
 			// name
 			if (challengees.isEmpty()) {
-				LOG.info("Party '" + challengerParty.getName() + "' wins this battle in " + roundCount + " rounds.");
+				history.add("Party '" + challengerParty.getName() + "' wins this battle in " + roundCount + " rounds.");
+				LOG.info(history.stream().collect(Collectors.joining("\n")));
 				return challengerParty;
 			}
 
 			if (challengers.isEmpty()) {
-				LOG.info("Party '" + challengeeParty.getName() + "' wins this battle in " + roundCount + " rounds.");
+				history.add("Party '" + challengeeParty.getName() + "' wins this battle in " + roundCount + " rounds.");
+				LOG.info(history.stream().collect(Collectors.joining("\n")));
 				return challengeeParty;
 			}
 		}
@@ -81,9 +90,9 @@ public class DefaultArenaService implements ArenaService {
 		Hero attacker = challenger;
 		Hero defender = challengee;
 
-		LOG.info(
+		history.add(
 				"------------------------------------------------------------------------------------------------------------");
-		LOG.info(attacker.getName() + " (ATK:" + attacker.getAtk() + ", DEF:" + attacker.getDef() + ") with "
+		history.add(attacker.getName() + " (ATK:" + attacker.getAtk() + ", DEF:" + attacker.getDef() + ") with "
 				+ f.format(attacker.getHp()) + " hp left fights against " + defender.getName() + " (ATK:"
 				+ defender.getAtk() + ", DEF:" + defender.getDef() + ") with " + f.format(defender.getHp())
 				+ " hp left.");
@@ -99,7 +108,7 @@ public class DefaultArenaService implements ArenaService {
 			// duel round
 			round(attacker, defender);
 		}
-		LOG.info(defender.getName() + " has lost the duel against " + attacker.getName() + ".");
+		history.add(defender.getName() + " has lost the duel against " + attacker.getName() + ".");
 		return defender;
 	}
 
@@ -111,21 +120,21 @@ public class DefaultArenaService implements ArenaService {
 		if (damage < 1)
 			damage++;
 		damage = damage / 10.0;
-		LOG.info("Attacking " + attacker.getName() + " caused " + damage + " damage.");
+		history.add("Attacking " + attacker.getName() + " caused " + damage + " damage.");
 
 		// defense blocks DEF percent of ATK
 		double defense = defender.getDef();
 		if (defense > 1)
 			defense--;
-		LOG.info("Defending " + defender.getName() + " blocked " + defense + " percent of damage.");
+		history.add("Defending " + defender.getName() + " blocked " + defense + " percent of damage.");
 
 		// harm = damage - defense
 		double harm = damage - (damage * (defense / 100));
-		LOG.info("Defending " + defender.getName() + " lost " + f.format(harm) + " of health.");
+		history.add("Defending " + defender.getName() + " lost " + f.format(harm) + " of health.");
 
 		// harm is drawn from defenders hp
 		defenderHp -= harm;
-		LOG.info("Defending " + defender.getName() + " has " + f.format(Math.max(0, defenderHp))
+		history.add("Defending " + defender.getName() + " has " + f.format(Math.max(0, defenderHp))
 				+ " health points left.");
 
 		defender.setHp(defenderHp);
